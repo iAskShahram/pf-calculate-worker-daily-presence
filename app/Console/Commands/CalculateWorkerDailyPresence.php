cb766f50c08bf8763b81dcc82530e13f78012b1e
Ahmed Shahram <shahram.ask.546@gmail.com>
2023-09-15 17:07:25 +0500

    sedded new offcie data to db and set grounds for console command

--- app/Console/Commands/CalculateWorkerDailyPresence.php
+++ app/Console/Commands/CalculateWorkerDailyPresence.php
@@ -3,30 +3,31 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
 class CalculateWorkerDailyPresence extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'command:name';
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'hr:calculatedailypresence';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Calculate worker daily presence';
 
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
-    public function handle()
-    {
-        return Command::SUCCESS;
-    }
+	/**
+	 * Execute the console command.
+	 *
+	 * @return int
+	 */
+	public function handle()
+	{
+		return Command::SUCCESS;
+	}
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,28 +10,22 @@ class DatabaseSeeder extends Seeder
 	public function run()
 	{
 		$OfficeIPs = [
-			[
-				"ip" => "233.243.435.6",
-				"name" => "Office 1"
-			],
-			[
-				"ip" => "174.246.324.7",
-				"name" => "Office 2"
-			],
-			[
-				"ip" => "165.345.243.251",
-				"name" => "Office 3"
-			],
-			[
-				"ip" => "143.432.234.11",
-				"name" => "Office 4"
-			]
+			['name' => 'Office A', 'ip' => '192.168.1.100'],
+			['name' => 'Office B', 'ip' => '10.0.0.1'],
+			['name' => 'Office C', 'ip' => '172.16.0.10'],
+			['name' => 'Office D', 'ip' => '192.168.2.50'],
+			['name' => 'Office E', 'ip' => '10.1.1.100'],
+			['name' => 'Office F', 'ip' => '172.17.0.5'],
+			['name' => 'Office G', 'ip' => '192.168.3.25'],
+			['name' => 'Office H', 'ip' => '10.2.2.200'],
+			['name' => 'Office I', 'ip' => '172.18.0.15'],
+			['name' => 'Office J', 'ip' => '192.168.4.75']
 		];
 		// add these offices to the database
 		foreach ($OfficeIPs as $OfficeIP) {
 			$office = new Office();
-			$office->ip = $OfficeIP['ip'];
 			$office->name = $OfficeIP['name'];
+			$office->ip = $OfficeIP['ip'];
 			$office->save();
 		}
 	}
