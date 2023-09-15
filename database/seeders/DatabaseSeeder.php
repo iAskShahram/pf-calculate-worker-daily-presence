874382ab2d11663d763be3a8405818757daadca5
Ahmed Shahram <shahram.ask.546@gmail.com>
2023-09-15 16:35:04 +0500

    migrate and run seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,23 +2,37 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use \App\Models\Office;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
+	public function run()
+	{
+		$OfficeIPs = [
+			[
+				"ip" => "233.243.435.6",
+				"name" => "Office 1"
+			],
+			[
+				"ip" => "174.246.324.7",
+				"name" => "Office 2"
+			],
+			[
+				"ip" => "165.345.243.251",
+				"name" => "Office 3"
+			],
+			[
+				"ip" => "143.432.234.11",
+				"name" => "Office 4"
+			]
+		];
+		// add these offices to the database
+		foreach ($OfficeIPs as $OfficeIP) {
+			$office = new Office();
+			$office->ip = $OfficeIP['ip'];
+			$office->name = $OfficeIP['name'];
+			$office->save();
+		}
+	}
 }
